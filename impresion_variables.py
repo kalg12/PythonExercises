e8d1ae39238fe14af5f6ae4572d3de24ec1e2a28
Kevin Luciano <78965771+kalg12@users.noreply.github.com>
2025-01-14 11:55:26 -0600

    Impresión de varibles

---
+++ impresion_variables.py
@@ -0,0 +1,15 @@
+""" Impresión de variables """
+nombre = "Kevin"
+print(nombre)
+
+numero_uno = 10
+numero_dos = 5
+
+""" Impresión de sumas """
+print(numero_uno+numero_dos)
+
+""" Impresión de concatenaciones """
+nombre_completo = "Kevin Armando"
+apellidos_completos= "Luciano García"
+
+print(nombre_completo + " " + apellidos_completos)
