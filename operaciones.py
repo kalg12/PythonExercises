3aac1eff7a21d05b40858a4f5d857951c5a67a1f
Kevin Luciano <78965771+kalg12@users.noreply.github.com>
2025-01-15 11:20:02 -0600

    Operaciones

---
+++ operaciones.py
@@ -0,0 +1,17 @@
+#Definición de dos variables en python para hacer calculos matemáticos
+
+#Definición de variables
+a = 10
+b = 8
+
+#Operaciones matemáticas
+suma = a + b
+resta = a - b
+multiplicacion = a * b
+division = a / b
+
+#Impresión de resultados
+print("La suma de a y b es: ", suma)
+print("La resta de a y b es: ", resta)
+print("La multiplicación de a y b es: ", multiplicacion)
+print("La división de a y b es: ", division)
